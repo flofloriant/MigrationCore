91e463901efa41bb7f0ef7a79c16dd62271590b9
Brochard Solene <solene.brochard@apside-groupe.com>
2021-03-24 09:30:53 +0100

    NewCollab vue

---
+++ Controllers/CollaboratorController.cs
@@ -0,0 +1,111 @@
+﻿
+using Apogee.Data;
+using Apogee.Models;
+using Apogee.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Apogee.SaveAs
+{
+    public static class FileSaveExtension
+    {
+        public static void SaveAs(this IFormFile formFile, string filePath)
+        {
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                formFile.CopyTo(stream);
+            }
+        }
+    }
+}
+namespace Apogee.Web.Helpers
+{
+    public static class ControllerExtensions
+    {
+        public static async Task<string> RenderViewAsync<TModel>(this Controller controller, string viewName, TModel model, bool partial = false)
+        {
+            if (string.IsNullOrEmpty(viewName))
+            {
+                viewName = controller.ControllerContext.ActionDescriptor.ActionName;
+            }
+
+            controller.ViewData.Model = model;
+
+            using (var writer = new StringWriter())
+            {
+                IViewEngine viewEngine = controller.HttpContext.RequestServices.GetService(typeof(ICompositeViewEngine)) as ICompositeViewEngine;
+                ViewEngineResult viewResult = viewEngine.FindView(controller.ControllerContext, viewName, !partial);
+
+                if (viewResult.Success == false)
+                {
+                    return $"A view with the name {viewName} could not be found";
+                }
+
+                ViewContext viewContext = new ViewContext(
+                    controller.ControllerContext,
+                    viewResult.View,
+                    controller.ViewData,
+                    controller.TempData,
+                    writer,
+                    new HtmlHelperOptions()
+                );
+
+                await viewResult.View.RenderAsync(viewContext);
+
+                return writer.GetStringBuilder().ToString();
+            }
+        }
+    }
+}
+
+namespace Apogee.Controllers
+{
+    using Apogee.SaveAs;
+    using Microsoft.AspNetCore.Mvc.Razor;
+    using Microsoft.AspNetCore.Mvc.ViewEngines;
+
+    [Authorize]
+    public class CollaboratorController : Controller
+    {
+        private readonly IDal dal;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly ApplicationDbContext _db;
+
+        public CollaboratorController(ApplicationDbContext db, IWebHostEnvironment webHostEnvironment)
+        {
+            _db = db;
+            _webHostEnvironment = webHostEnvironment;
+            
+        }
+        
+
+        #region NewCollaborateur en mode Get
+        /// <summary>
+        /// Création d'un collaboratuer par default
+
+        /// </summary>
+        /// <returns></returns>
+        /// [ValidateAntiForgeryToken]
+        /// 
+
+        [HttpGet]
+        public ActionResult NewCollab()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
---
+++ Models/CollaboratorViewComponent.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Apogee.Models
+{
+    public class CollaboratorViewComponent : ViewComponent
+    {
+        private IDal dal;
+
+        public IViewComponentResult Invoke()
+        {
+            #region Importer Exporter une photo
+            #region PhotoVue Importer en mode Get
+            /// <summary>
+            /// Vue photo pour importer ou exporter photo
+            /// </summary>
+            /// <param name="model"></param>
+            /// <returns></returns>
+            /*Collaborateur collaborateur = dal.RecupererCollaborateur(9);*/
+            Collaborateur collaborateur = new Collaborateur();
+            Photo photo = new Photo();
+                photo.Erreur = string.Empty;
+                photo.Fic_photo_path = collaborateur.Fic_photo;
+                photo.Id = collaborateur.Id;
+                photo.Fic_autorisation_photoCollab = collaborateur.Fic_autorisation_photo;
+                photo.Fic_photoCollab = collaborateur.Fic_photo;
+                return View("PhotoVue", photo);
+            #endregion
+            #endregion
+        }
+    }
+}
